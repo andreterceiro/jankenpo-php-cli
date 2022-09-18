d155a1dc9dea3108f3cbccabe24f3125ab369834
André de Paula Terceiro <andre@terceiro.com.br>
2022-09-18 13:30:59 -0300

    Other tests

--- Jankenpo.php
+++ Jankenpo.php
@@ -6,34 +6,60 @@ class Jankenpo {
     const ROCK = 2;
     const SCISSOR = 3;
 
+    const CHARACTER_PAPER = 'p';
+    const CHARACTER_ROCK = 'r';
+    const CHARACTER_SCISSOR = 's';
+
+    const INTEGER_COMPUTER_WINS = -1;
+    const INTEGER_DRAW = 0;
+    const INTEGER_PLAYER_WINS = 1;
+
     /**
      * Process the user input and says if the winner is the user,
      * the computer or is a draw
      * 
      * @access public     
      *
-     * @param string $characterUserOption The user option
+     * @param string  $characterUserOption The user option
+     * @param boolean [$quiet=true]        If true, the result is not echoed
      *   
      * @return null
      */
-    public function process($characterUserOption) {
+    public function process($characterUserOption, $quiet = true) {
         $integerUserOption = $this->convertCharacterOptionToIntegerOption($characterUserOption);
         unset($characterUserOption);
 
-        $integerComputerOption = random_int(1, 3);
+        $integerComputerOption = $this->getIntegerComputerChoice();
 
         $stringUserOption = $this->convertIntegerOptionToStringOption($integerUserOption);
         $stringComputerOption = $this->convertIntegerOptionToStringOption($integerComputerOption);
 
+        $dataToReturn = self::INTEGER_DRAW; 
         if (($integerUserOption == 1 && $integerComputerOption == 2) || ($integerUserOption == 2 && $integerComputerOption == 3) || ($integerUserOption == 3 && $integerComputerOption == 1)) {
             $winnerStringMessage = "player wins";
+            $dataToReturn = self::INTEGER_PLAYER_WINS;
         } else if ($integerUserOption == $integerComputerOption) {
             $winnerStringMessage = "draw";
         } else {
             $winnerStringMessage = "computer wins";
+            $dataToReturn = self::INTEGER_COMPUTER_WINS;
         }
 
-        echo "Player selected '$stringUserOption' ('$stringUserOption[0]') and computer selected '$stringComputerOption' ($stringComputerOption[0]) - $winnerStringMessage\n";
+        if (! $quiet) {
+            echo "Player selected '$stringUserOption' ('$stringUserOption[0]') and computer selected '$stringComputerOption' ($stringComputerOption[0]) - $winnerStringMessage\n";
+        }
+        return $dataToReturn;
+    }
+
+    /**
+     * Returns an integer (1 to 3) related to a random computer choice
+     *
+     * @access public
+     *
+     * @return integer
+     */
+    public function getIntegerComputerChoice() {
+        return random_int(1, 3);
     }
 
     /**
@@ -48,19 +74,19 @@ class Jankenpo {
      * @return int
      */
     public function convertCharacterOptionToIntegerOption($option) {
-        if ($option == 'p') {
+        if ($option == self::CHARACTER_PAPER) {
             return self::PAPER;
         }
         
-        if ($option == 'r') {
+        if ($option == self::CHARACTER_ROCK) {
             return self::ROCK;
         }
 
-        if ($option == 's') {
+        if ($option == self::CHARACTER_SCISSOR) {
             return self::SCISSOR;
         }
 
-        throw new \OutOfBoundsException("The option is not 'p', 'r' or 's'");
+        throw new \OutOfBoundsException("The option is not '" . self::CHARACTER_PAPER . "', " . self::CHARACTER_ROCK . " or '" . self::CHARACTER_SCISSOR . "'");
     }
 
     /**
@@ -74,16 +100,16 @@ class Jankenpo {
      * 
      * @return string
      */
-    public function convertIntegerOptionToStringOption($option) {
-        if ($option == self::PAPER) {
+    public function convertIntegerOptionToStringOption($integerOption) {
+        if ($integerOption == self::PAPER) {
             return 'paper';
         }
         
-        if ($option == self::ROCK) {
+        if ($integerOption == self::ROCK) {
             return 'rock';
         }
 
-        if ($option == self::SCISSOR) {
+        if ($integerOption == self::SCISSOR) {
             return 'scissor';
         }
 
--- tests/JankenpoTest.php
+++ tests/JankenpoTest.php
@@ -134,5 +134,204 @@ class JankenpoTest extends \PHPUnit\Framework\TestCase {
         );
 
         $this->Jankenpo->convertIntegerOptionToStringOption(4);
-    }    
+    }
+    
+    /**
+     * Test of match:
+     * User: Paper
+     * Computer: Paper
+     * 
+     * Result: Draw
+     * 
+     * @access public
+     * 
+     * @return null
+     */
+    public function testProcessUserPaperComputerPaperDraw() {
+        $mock = $this->setupProcessTest(Jankenpo::PAPER);
+
+        $this->assertEquals(
+            $mock->process(Jankenpo::CHARACTER_PAPER),
+            Jankenpo::INTEGER_DRAW
+        );
+    }
+
+    /**
+     * Test of match:
+     * User: Paper
+     * Computer: Rock
+     * 
+     * Result: Player wins
+     * 
+     * @access public
+     * 
+     * @return null
+     */
+    public function testProcessUserPaperComputerRockUserWins() {
+        $mock = $this->setupProcessTest(Jankenpo::ROCK);
+
+        $this->assertEquals(
+            $mock->process(Jankenpo::CHARACTER_PAPER),
+            Jankenpo::INTEGER_PLAYER_WINS
+        );
+    }
+
+    /**
+     * Test of match:
+     * User: Paper
+     * Computer: Scissor
+     * 
+     * Result: Computer wins
+     * 
+     * @access public
+     * 
+     * @return null
+     */
+    public function testProcessUserPaperComputerScissorComputerWins() {
+        $mock = $this->setupProcessTest(Jankenpo::SCISSOR);
+
+        $this->assertEquals(
+            $mock->process(Jankenpo::CHARACTER_PAPER),
+            Jankenpo::INTEGER_COMPUTER_WINS
+        );
+    }
+
+    /**
+     * Test of match:
+     * User: Rock
+     * Computer: Paper
+     * 
+     * Result: Computer wins
+     * 
+     * @access public
+     * 
+     * @return null
+     */
+    public function testProcessUserRockComputerPaperComputerWins() {
+        $mock = $this->setupProcessTest(Jankenpo::PAPER);
+
+        $this->assertEquals(
+            $mock->process(Jankenpo::CHARACTER_ROCK),
+            Jankenpo::INTEGER_COMPUTER_WINS
+        );
+    }
+
+    /**
+     * Test of match:
+     * User: Rock
+     * Computer: Rock
+     * 
+     * Result: Draw
+     * 
+     * @access public
+     * 
+     * @return null
+     */
+    public function testProcessUserRockComputerRockDraw() {
+        $mock = $this->setupProcessTest(Jankenpo::ROCK);
+
+        $this->assertEquals(
+            $mock->process(Jankenpo::CHARACTER_ROCK),
+            Jankenpo::INTEGER_DRAW
+        );
+    }
+
+    /**
+     * Test of match:
+     * User: Rock
+     * Computer: Scissor
+     * 
+     * Result: Player wins
+     * 
+     * @access public
+     * 
+     * @return null
+     */
+    public function testProcessUserRockComputerScissorUserWins() {
+        $mock = $this->setupProcessTest(Jankenpo::SCISSOR);
+
+        $this->assertEquals(
+            $mock->process(Jankenpo::CHARACTER_ROCK),
+            Jankenpo::INTEGER_PLAYER_WINS
+        );
+    }
+
+    /**
+     * Test of match:
+     * User: Scissor
+     * Computer: Paper
+     * 
+     * Result: Player wins
+     * 
+     * @access public
+     * 
+     * @return null
+     */
+    public function testProcessUserScissorComputerPaperPlayerWins() {
+        $mock = $this->setupProcessTest(Jankenpo::PAPER);
+
+        $this->assertEquals(
+            $mock->process(Jankenpo::CHARACTER_SCISSOR),
+            Jankenpo::INTEGER_PLAYER_WINS
+        );
+    }
+
+    /**
+     * Test of match:
+     * User: Scissor
+     * Computer: Rock
+     * 
+     * Result: Computer wins
+     * 
+     * @access public
+     * 
+     * @return null
+     */
+    public function testProcessUserScissorComputerRockPlayerWins() {
+        $mock = $this->setupProcessTest(Jankenpo::ROCK);
+
+        $this->assertEquals(
+            $mock->process(Jankenpo::CHARACTER_SCISSOR),
+            Jankenpo::INTEGER_COMPUTER_WINS
+        );
+    }
+
+    /**
+     * Test of match:
+     * User: Scissor
+     * Computer: Scissor
+     * 
+     * Result: Draw
+     * 
+     * @access public
+     * 
+     * @return null
+     */
+    public function testProcessUserScissorComputerScissorDraw() {
+        $mock = $this->setupProcessTest(Jankenpo::SCISSOR);
+
+        $this->assertEquals(
+            $mock->process(Jankenpo::CHARACTER_SCISSOR),
+            Jankenpo::INTEGER_DRAW
+        );
+    }
+
+    /**
+     * Auxiliar method to other tests
+     * 
+     * @access public
+     * 
+     * @return Mock_Jankenpo
+     */
+    public function setupProcessTest($integerComputerChoice) {
+        $mock = $this->getMockBuilder(\andredepaulaterceiro\Jankenpo::class)
+            ->onlyMethods(['getIntegerComputerChoice'])
+            ->getMock();
+
+        $mock->expects($this->exactly(1))
+            ->method('getIntegerComputerChoice')
+            ->will($this->returnValue($integerComputerChoice));
+
+        return $mock;
+    }
 }
